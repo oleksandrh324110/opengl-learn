b52f33bb1f6a6c6dc4b4b9b00f45af3b27396c18
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-02-21 15:27:28 +0200

    .

--- src/util/util.h
+++ src/util/util.h
@@ -11,9 +11,9 @@
 #include <assert.h>
 #include <limits.h>
 
+#include <cglm/mat4.h>
 #include <cglm/cglm.h>
 #include <cglm/struct.h>
-#include <cglm/mat4.h>
 #include <cglm/vec2.h>
 #include <cglm/vec3.h>
 #include <cglm/vec4.h>
